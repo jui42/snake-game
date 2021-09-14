e9277e7a85c73557cd0dd41bd9186e35d4ec2d46
Jui <juliopaimfotografia@gmail.com>
2021-09-14 02:31:39 -0300

    commit final
    *Por enquanto.

--- snake-game-code/script.js
+++ snake-game-code/script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box, 
     y: 8 * box  
 }
+
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
@@ -16,21 +17,21 @@ function criarBG() {
     context.fillStyle = "black"; 
     context.fillRect(0, 0, 16 * box, 16 * box);  
 }
-
+//criar gray point inicial
 function criarSnake() {
     for(i=0; i < snake.length; i++) {
         context.fillStyle = "grey";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-
+// Gerar red point aleatoriamente a cada F5
 function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
+//captando entrada do teclado
 document.addEventListener('keydown', update);
-
 function update (event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
@@ -43,6 +44,13 @@ function iniciarGame(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :(');
+        }
+    }
    
     criarBG();
     criarSnake();
@@ -64,18 +72,12 @@ function iniciarGame(){
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
     
-
-
-  
-
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
     snake.unshift(newHead);
-
-
 }
 
 let game = setInterval(iniciarGame, 100);
